e65dad7bf597a208403f66579fdddc3928fda26e
awelnerhissen <awelinsa@gmail.com>
2018-05-11 12:11:12 +0200

    Check for input has been added
    Check for input data has been added and variable moved
    to separate file, inc/memberRegistrationVariables.php

--- lib/memeberManagement.php
+++ lib/memeberManagement.php
@@ -45,6 +45,157 @@ class MemberManagement{
         return $data;
     }
     
+    /**
+     *
+     * Check validty of the input data
+     * @param String $input
+     * @param String $inputName
+     * @param int $validationType
+     *        1 Only letters and white space allowed
+     *        2 Only numbers and - allowed
+     *        3 Only numbers - and white space allowed
+     *        4 Only numbers - + and white space allowed
+     *        10 Check only if the input is empty if empty is not allowed.
+     * @param String $errorMessag replace error message by this when the input data is invalid.
+     * @param boolean $checkEmpty
+     * @param String $example add this exmaple to error when the input data is invalid
+     * @return string
+     */
+    
+    function checkInputData($input, $inputName, $validationType, $checkEmpty,  $errorMessag="", $example=""){
+        $ONLY_LETTERS_AND_WHITE_SPACE = "/^[a-zA-ZåäöÅÄÖ ]*$/";
+        $ONLY_NUMBERS_AND_HYPHEN = "/^[0-9-]*$/";
+        $ONLY_NUMBERS_AND_WHITE_SPACE_AND_HYPHEN = "/^[0-9- ]*$/";
+        $ONLY_NUMBERS_AND_WHITE_SPACE_AND_HYPHEN_AND_PLUS = "/^[0-9-+ ]*$/";
+        
+        
+        $strError = "";
+        $regularExpression ="";
+        $errorMessageWhenInvalid="";
+        if ($checkEmpty && empty($input)) {
+            $strError = $inputName. " is required";
+        } else if ($validationType!=10){
+            if($validationType==1){
+                $regularExpression = $ONLY_LETTERS_AND_WHITE_SPACE;
+                $errorMessageWhenInvalid="Only letters and white space allowed";
+            }else if($validationType==2){
+                $regularExpression = $ONLY_NUMBERS_AND_HYPHEN;
+                $errorMessageWhenInvalid="Only numbers and - allowed";
+            }else if($validationType==3){
+                $regularExpression = $ONLY_NUMBERS_AND_WHITE_SPACE_AND_HYPHEN;
+                $errorMessageWhenInvalid="Only numbers - and white space allowed";
+            }else if($validationType==4){
+                $regularExpression = $ONLY_NUMBERS_AND_WHITE_SPACE_AND_HYPHEN_AND_PLUS;
+                $errorMessageWhenInvalid="Only numbers - + and white space allowed";
+            }
+            
+            
+            if($errorMessag !=""){
+                $errorMessageWhenInvalid = $errorMessag;
+            }
+            
+            if($example !=""){
+                $errorMessageWhenInvalid .= " e.g ". $example;
+            }
+            // check if name only contains letters and whitespace
+            if (!preg_match($regularExpression,$input)) {
+                $strError = $errorMessageWhenInvalid;
+            }
+        }
+        return $strError;
+    }
+    
+    /**
+     * Validate if sex has been selected/filled
+     * @param String $selectedSex
+     * @return string
+     */
+    function checkSelectedSex($selectedSex){
+        $strError="";
+        if($selectedSex=="0"){
+            $strError="Fill your sex.";
+        }
+        return $strError;
+    }
+    
+    /**
+     * Check if email is valid email. Skip checking email if $checkEmail 
+     * is false and $email is empy.
+     * 
+     * @param String $email
+     * @param boolean $checkEmpty
+     */
+    function checkEmail($email, $checkEmpty){
+        $validateEmail = true;
+        $emailErr="";
+        if (!$checkEmpty && empty($email)){
+            $validateEmail = false;
+        }
+        if($validateEmail){
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Invalid email format";
+            }
+        }
+        return $emailErr;
+    }
+    
+   /**
+    * 
+    * @param String $number
+    * @param String $checkEmpty
+    */
+    function checkMobileNumber($number, $checkEmpty){
+        $ONLY_NUMBERS_AND_WHITE_SPACE_AND_HYPHEN_AND_PLUS = "/^[0-9-+ ]*$/";
+        $validateNumber = true;
+        $minimumMobileOrTelephoneNumberLength = 10; //e.g 0735618xxxx
+        $numberError="";
+        if (!$checkEmpty && empty($minimumMobileOrTelephoneNumberLength)){
+            $validateNumber = false;
+        }
+        if($validateNumber){
+            if(strlen($number)<10){
+                $numberError="Invalid number";
+                return $numberError;
+            }
+            if( !preg_match($ONLY_NUMBERS_AND_WHITE_SPACE_AND_HYPHEN_AND_PLUS,$number)){
+                $numberError="Only numbers - + and white space allowed";
+                return $numberError;
+            }
+        }
+        return $numberError;
+        
+    }
+    
+    /**
+     * Validate if passowrd fullfills some requirements.
+     * @param String $password
+     * @param String $confirmPassword
+     * @param boolean $useStrongPass
+     * @return string
+     */
+    function checkPassword($password, $confirmPassword, $useStrongPass){
+        //&&!preg_match( $digitsRegExp, $password) && !preg_match($specialCharacterRegExp, $password)
+        //&& !preg_match( $alphabetsRegExp, $password) && !preg_match( $digitsRegExp, $password)
+        $strErr =  "";
+        $alphabetsRegExp='/[a-zA-Z]+/'; //match characters a to z or A to Z
+        $digitsRegExp='/\d+/'; //match digits
+        $specialCharacterRegExp ='/\W+/';
+        if(strlen($password)<6){
+            $strErr ="Password length at least 6 characters.";
+            return $strErr;
+        }
+        if($useStrongPass  && (!preg_match( $alphabetsRegExp, $password) || !preg_match( $digitsRegExp, $password) || !preg_match( $specialCharacterRegExp, $password))){
+                $strErr ="Password must have at least one character, one digit and one special symbol.";
+                return $strErr;
+        }
+        if($password != $confirmPassword){
+            $strErr ="Password and Confirm password are not the same.";
+            return $strErr;
+        }
+        
+        
+    }
+    
     
     /**
      * Get list of marital status in prepared select form.
@@ -107,5 +258,6 @@ class MemberManagement{
          return $result;
      }
     }
+    
 }
 ?>
--- main/memberRegistration.php
+++ main/memberRegistration.php
@@ -1,28 +1,7 @@
 <?php 
 	require_once '../lib/memeberManagement.php';
  	require_once 'config.php';
- 
- 	$personalNumber;
- 	$dateOfBirth;
- 	$firstName;
- 	$fatherName;
- 	$gFatherName;
- 	$sex;
- 	$maritalStatus=0;
- 	$residenceStatus=0;
- 	$primaryEmail;
- 	$email2;
- 	$password;
- 	$confirmPassword;
- 	$mobileNumber;
- 	$mobileNumber2;
- 	$telephoneNumber;
- 	$city;
- 	$streetAddress;
- 	$poBox;
- 	$kommun;
- 	$country;
- 	
+ 	require_once 'inc/memberRegistrationVariables.php';
  	
  	if($_SERVER["REQUEST_METHOD"] == "POST") {
  	    $personalNumber = $memberManagement->test_input($_POST["txtPersonalNumber"]);
@@ -45,7 +24,29 @@
  	    $poBox= $memberManagement->test_input($_POST["txtPoBox"]);
  	    $kommun= $memberManagement->test_input($_POST["txtKommun"]);
  	    $country = $memberManagement->test_input($_POST["txtCountry"]);
+ 	    
+ 	    $PersonalNumberErr =  $memberManagement->checkInputData($personalNumber, "Personal number", 2, true);
+ 	    $dateOfBirthlErr = $memberManagement->checkInputData($dateOfBirth, "Date of birth", 2, true, "", "19671225 or 1967-12-25");
+ 	    $firstNameErr = $memberManagement->checkInputData($firstName, "First name", 1, true);
+ 	    $fatherNameErr = $memberManagement->checkInputData($fatherName, "Father name", 1, false);
+ 	    $gFatherNameErr = $memberManagement->checkInputData($gFatherName, "G.Father/Last name", 1, true);
+ 	    $sexErr=$memberManagement->checkSelectedSex($sex);
+ 	    $primaryEmailErr = $memberManagement->checkEmail($primaryEmail, true);
+ 	    $email2Err = $memberManagement->checkEmail($email2, false);
+ 	    $ConfirmPasswordErr=$passwordErr = $memberManagement->checkPassword($password, $confirmPassword, true);
+ 	    $mobileNumberErr = $memberManagement->checkMobileNumber($mobileNumber,true);
+ 	    $mobileNumber2Err  = $memberManagement->checkMobileNumber($mobileNumber2, false);
+ 	    $telephoneNumberErr  = $memberManagement->checkMobileNumber($telephoneNumber, false);
+ 	    
+ 	    $cityErr =$memberManagement->checkInputData($city, "City", 1, true);
+ 	    $streetAddressErr = $memberManagement->checkInputData($streetAddress, "Street address", 10, true);
+ 	    $poBoxErr = $memberManagement->checkInputData($poBox, "Po.Box", 3, true);
+ 	    $kommunErr = $memberManagement->checkInputData($kommun, "Kommun", 1, true);
+ 	    
+ 	    
+ 	    
  	}
+ 	
 ?>
 
 <!DOCTYPE html>
@@ -76,7 +77,7 @@
   <label for="txtFatherName">Father name:</label> <input type="text" name="txtFatherName" value="<?php echo $fatherName;?>">
   <span class="error"><?php echo $fatherNameErr;?></span>
   <br><br>
-  <label for="txtGFatherName">G.Father name:</label> <input type="text" name="txtGFatherName" value="<?php echo $gFatherName;?>">
+  <label for="txtGFatherName">G.Father/Last name:</label> <input type="text" name="txtGFatherName" value="<?php echo $gFatherName;?>">
   <span class="error"><?php echo $gFatherNameErr;?></span>
   <br><br>
   <label for="selectSex">Sex</label> 
@@ -84,8 +85,8 @@
   <option value="0"<?php if(isset($sex) && $sex=="0") echo "selected = 'selected'";?>></option>
   <option value="1"<?php if(isset($sex) && $sex=="1") echo "selected = 'selected'";?>>Male</option>
   <option value="2"<?php if(isset($sex) && $sex=="2") echo "selected = 'selected'";?>>Female</option>
-  <?php if (isset($gender) && $gender=="female") echo "checked";?>
 </select>
+<span class="error"><?php echo $sexErr;?></span>
    <br><br>
     <label for="selectMeritalStatus">Meriatal Status:</label> 
    <?php echo $memberManagement->getMeritalStatusSelect($maritalStatus);?>
@@ -97,6 +98,7 @@
   <span class="error"><?php echo $primaryEmailErr;?></span>
   <br><br>
     <label for="email2">Email2:</label> <input type="email" name="email2" value="<?php echo $email2;?>">
+  <span class="error"><?php echo $email2Err;?></span>
   <br><br>
     <label for="password">Password:</label>  <input type="password" name="password" value="<?php echo $password;?>">
   <span class="error"><?php echo $passwordErr;?></span>
@@ -108,8 +110,10 @@
   <span class="error"><?php echo $mobileNumberErr;?></span>
   <br><br>
      <label for="mobileNumber2">Mobile number2:</label><input type="text" name="txtMobileNumber2" value="<?php echo $mobileNumber2;?>">
+  <span class="error"><?php echo $mobileNumber2Err;?></span>
   <br><br>
    <label for="telephoneNumber">Telephone number:</label> <input type="text" name="txtTelephoneNumber" value="<?php echo $telephoneNumber;?>">
+  <span class="error"><?php echo $telephoneNumberErr;?></span>
   <br><br>
    <!-- <fieldset style="width:330px"> -->
    <fieldset style="width:100%">
@@ -124,6 +128,7 @@
   <span class="error"><?php echo $poBoxErr;?></span>
   <br><br>
         <label for="txtKommun">Kommun:</label> <input type="text" name="txtKommun" value="<?php echo $kommun;?>">
+   <span class="error"><?php echo $kommunErr;?></span>
   <br><br>
         <label for="Country:">Country:</label> <input type="text" name="txtCountry" value="Sweden" disabled="disabled">
         </fieldset>
